e0d19e25a117e9fd908d9df5e9b538394c637ffb
“fit-helder” <helder.lima@fitbank.com.br>
2023-09-02 23:55:47 -0300

    feat: add get .env variables

--- main.go
+++ main.go
@@ -4,10 +4,17 @@ import (
 	"Api-Rest/database"
 	"Api-Rest/routes"
 	"log"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Erro ao carregar o arquivo .env")
+	}
+
 	database.DBConnect()
+
 	log.Print("Starting Rest Server in Go")
 	routes.HandleRequest()
 }
