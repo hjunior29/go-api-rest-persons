3d59b1a70f92ef450dec20311fe35d1971109a8d
“fit-helder” <helder.lima@fitbank.com.br>
2023-09-02 23:55:20 -0300

    feat: adapting db connect to get .env variables

--- database/db.go
+++ database/db.go
@@ -1,7 +1,9 @@
 package database
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -13,7 +15,13 @@ var (
 )
 
 func DBConnect() {
-	connection := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable"
+	connection := fmt.Sprintf("user=%s dbname=%s password=%s host=%s sslmode=%s",
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_SSLMODE"),
+	)
 	DB, err = gorm.Open(postgres.Open(connection))
 	if err != nil {
 		log.Panic("Database connection error")
